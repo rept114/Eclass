d8b0e17add4ea6279e459d82804848ba0f2d5fa4
Fer <reikafer@gmail.com>
2022-09-07 08:51:11 -0700

    mensaje

--- englishclass/ViewController.swift
+++ englishclass/ViewController.swift
@@ -11,11 +11,25 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var Name: UILabel!
     
+    @IBOutlet weak var LabelTitle: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func DoTapImage1(_ sender: Any) {
+        LabelTitle.text = "Perro"
+    }
+    @IBAction func DoTapImage2(_ sender: Any) {
+        LabelTitle.text = "Gato"
+    }
+    @IBAction func DoTapImage3(_ sender: Any) {
+        LabelTitle.text = "Perry"
+    }
+    @IBAction func DoTapImage4(_ sender: Any) {
+        LabelTitle.text = "Naomi"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
